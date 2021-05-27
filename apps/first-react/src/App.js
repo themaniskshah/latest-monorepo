9fc0a25c12b8e612fbc53a07e83b9d9cf3a9d46e
themaniskshah <mkshah987@gmail.com>
2021-05-28 02:47:58 +0530

    mono repo Test

---
+++ apps/first-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Thing } from "components"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Thing />
+    </div>
+  );
+}
+
+export default App;
